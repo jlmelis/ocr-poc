287fb6c8be4f6db47d87957f19ee0eb9a54a5339
jlmelis <jason.melis@gmail.com>
2022-02-09 18:03:22 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import logo from './assets/svelte.png'
+  import Camera from './lib/Camera.svelte'
+</script>
+
+<main>
+  <Camera></Camera>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Camera.svelte
@@ -0,0 +1,19 @@
+<script>
+    let videoStream = null;
+    let video;
+
+    async function startCamera() {
+        videoStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false});
+        video.srcObject = videoStream;
+    }
+
+    function stopCamera() {
+        videoStream.getTracks().forEach(track => track.stop());
+    }
+</script>
+
+<section>
+    <video bind:this="{video}" width="320" height="240" autoplay ><track kind="captions"></video>
+    <button class="btn btn-primary" on:click="{startCamera}">Start Camera</button>
+    <button class="btn btn-primary" on:click="{stopCamera}">Stop Camera</button>
+</section>
